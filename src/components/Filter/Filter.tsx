c02a86891c28bb87494ee5e12ce77a1a06267e05
Adam Ciołek <adasciolek95@gmail.com>
2022-05-25 18:28:12 +0200

    adapting categories to mobile devices

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -4,13 +4,42 @@ import FilterPrice from "../../features/Filter/FilterPrice";
 
 import { allBooks } from "../../features/Filter/filterSlice";
 import { useDispatch } from "react-redux";
+import { FC } from "react";
 
-const Filter = () => {
+interface Open {
+	open: boolean;
+	width: number;
+}
+
+const Filter: FC<Open> = ({ open, width }) => {
 	const dispatch = useDispatch();
 
 	const handleGiveBooks = () => {
 		dispatch(allBooks());
 	};
+
+	if (width < 1024) {
+		return (
+			<section>
+				{open && (
+					<div className="absolute bg-white w-full z-10 pb-6">
+						<h2 className="mt-8 text-2xl" onClick={handleGiveBooks}>
+							Język
+						</h2>
+						<FilterLanguage />
+						<h2 className="mt-8 text-2xl" onClick={handleGiveBooks}>
+							Książki
+						</h2>
+						<FilterCategory />
+						<h2 className="mt-8 text-2xl" onClick={handleGiveBooks}>
+							Cena
+						</h2>
+						<FilterPrice />
+					</div>
+				)}
+			</section>
+		);
+	}
 
 	return (
 		<section>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Filter } from "../components";
 import { MdOutlineExpandMore } from "react-icons/md";
 // components
@@ -7,19 +8,33 @@ import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
 
 const Home = () => {
+	const [open, setOpen] = useState<boolean>(false);
+	const [width, setWidth] = useState<number>(window.innerWidth);
+
 	const books = useSelector((state: RootState) => state.filter.items);
+
+	useEffect(() => {
+		const handleResize = () => setWidth(window.innerWidth);
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	});
 
 	return (
 		<main className="mt-12 relative overflow-hidden">
 			<div className="px-8 lg:flex lg:justify-items-start justify-between max-w-screen-2xl mx-auto">
 				<div>
-					<h2 className="text-3xl tracking-wider flex items-center cursor-pointer lg:cursor-auto w-max">
+					<h2
+						className="text-3xl tracking-wider flex items-center cursor-pointer lg:cursor-auto w-max"
+						onClick={() => setOpen(!open)}
+					>
 						Kategoria
 						<span className="ml-2 lg:hidden">
 							<MdOutlineExpandMore />
 						</span>
 					</h2>
-					<Filter />
+					<Filter open={open} width={width} />
 				</div>
 				<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 justify-items-center my-12">
 					{books.map((book) => {
