f3a1709a2b5ae5284a63018231c8e31af7d5d8a3
Adam Ciołek <adasciolek95@gmail.com>
2022-06-20 18:57:12 +0200

    add filter

--- src/features/Filter/filterSlice.ts
+++ src/features/Filter/filterSlice.ts
@@ -25,7 +25,73 @@ const initialState: FilterBooks = {
 const filterSlice = createSlice({
 	name: "filter",
 	initialState,
-	reducers: {},
+	reducers: {
+		filter: (state) => {
+			const { all_items } = state;
+			const { bookName, bookLanguage, min_price, max_price } = state.filter;
+
+			let tempProducts = [...all_items];
+
+			// category
+			if (bookName) {
+				tempProducts = tempProducts.filter(
+					(book) => book.category === bookName
+				);
+			}
+			// language
+			if (bookLanguage) {
+				tempProducts = tempProducts.filter(
+					(book) => book.edition === bookLanguage
+				);
+			}
+
+			// price
+			if (max_price && min_price) {
+				tempProducts = tempProducts.filter(
+					(product) => product.price >= min_price && product.price <= max_price
+				);
+			}
+
+			state.items = tempProducts;
+		},
+
+		filterLanguage: (state, action: PayloadAction<Language>) => {
+			state.filter.bookLanguage = action.payload.name;
+			state.filter.checked = action.payload.checked as boolean;
+
+			if (!action.payload.checked) {
+				state.filter.bookLanguage = "";
+				state.filter.checked = false;
+			}
+		},
+		filterPrice: (state, action: PayloadAction<Price>) => {
+			state.filter.min_price = action.payload.minPrice;
+			state.filter.max_price = action.payload.maxPrice;
+			state.filter.checkedPrice = action.payload.checked as boolean;
+
+			if (!action.payload.checked) {
+				state.filter.min_price = 0;
+				state.filter.max_price = 0;
+			}
+		},
+		filterCategory: (state, action: PayloadAction<string | undefined>) => {
+			state.filter.bookName = action.payload;
+		},
+	},
 });
+
+export const allBooksStorage = (state: RootState) => state.filter.all_items;
+export const bookNames = (state: RootState) => state.filter.filter.bookName;
+export const bookLanguages = (state: RootState) => state.filter.all_items;
+export const checkedValues = (state: RootState) => state.filter.filter.checked;
+export const bookLangs = (state: RootState) => state.filter.filter.bookLanguage;
+
+export const minPrices = (state: RootState) => state.filter.filter.min_price;
+export const maxPrices = (state: RootState) => state.filter.filter.max_price;
+export const checkedPrice = (state: RootState) =>
+	state.filter.filter.checkedPrice;
+
+export const { filterCategory, filterLanguage, filterPrice, filter } =
+	filterSlice.actions;
 
 export default filterSlice.reducer;
