400b42c723fb4e3c902200aebd9927f071627b6e
Adam Ciołek <adasciolek95@gmail.com>
2022-07-07 21:55:45 +0200

    config eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import { Route, Routes } from "react-router";
 // import pages
 import { Layout, Home, SingleBook, Error, Cart } from "./pages";
 
-const App = () => {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="cart" element={<Cart />} />
-					<Route path="product/:id" element={<SingleBook />} />
-					<Route path="*" element={<Error />} />
-				</Route>
-			</Routes>
-		</>
-	);
-};
+const App = () => (
+	<Routes>
+		<Route path="/" element={<Layout />}>
+			<Route index element={<Home />} />
+			<Route path="cart" element={<Cart />} />
+			<Route path="product/:id" element={<SingleBook />} />
+			<Route path="*" element={<Error />} />
+		</Route>
+	</Routes>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { store } from "./app/store";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement,
 );
 root.render(
 	<React.StrictMode>
@@ -16,5 +17,5 @@ root.render(
 				<App />
 			</Router>
 		</Provider>
-	</React.StrictMode>
+	</React.StrictMode>,
 );
