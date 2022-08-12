47f13be46886b9f8cb8da36e8b7fa22d6c749b60
Adam Ciołek <adasciolek95@gmail.com>
2022-08-12 14:08:28 +0200

    fix bug

--- dist/client/src/components/shared/Container/Container.jsx
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Container = ({ children }) => (<div className="mt-12 max-w-screen-2xl mx-auto px-8">{children}</div>);
-exports.default = Container;
--- dist/client/src/pages/Layout.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_router_1 = require("react-router");
-const components_1 = require("../components");
-const Layout = () => (<div>
-		<components_1.Header />
-		<react_router_1.Outlet />
-	</div>);
-exports.default = Layout;
