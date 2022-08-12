7a1a225780e3a77303fbbfe982a71aebd25ce1cf
Adam Ciołek <adasciolek95@gmail.com>
2022-08-12 12:18:57 +0200

    add route auth

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+require("express-async-errors");
 import connect from "./db/connect";
+import router from "./routes/authentication";
+import bodyParser from "body-parser";
 import routeNotFound from "./middleware/routeNotFound";
 import errorHandler from "./middleware/errorHandler";
+import "dotenv/config";
 
 const connectionDB = process.env.MONGO_URI as string;
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+// routers
+app.use("/api/v1", router);
 
 // display 404 page if route doesn't exist
 app.use(routeNotFound);
